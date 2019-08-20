ac9243a2bc0b60083ac2dae9b4dd93f6f2e9098a
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-08-20 18:19:06 -0400

    post review

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 
 source "https://rubygems.org"
 
-gem 'pry-byebug'
 gem 'require_all'
+gem 'pry'
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+flatiron = Bakery.new("Flatiron Bakery")
+yummy_time = Bakery.new("Yummy Time")
+
+cake = Dessert.new("Chocolate Cake", flatiron)
+cookies = Dessert.new("Cookies", yummy_time)
+
+egg = Ingredient.new("Egg", 50, cake)
+flour = Ingredient.new("Flour", 100, cake)
+chocolate = Ingredient.new("Chocolate", 200, cake)
+
+chips = Ingredient.new("Chips", 100, cookies)
+peanuts = Ingredient.new("Peanuts", 50, cookies)
+
+# chips.bakery
+
+
+binding.pry
+0
